98164af921f377e5d3a58f0d7a853367378712c3
weijie9 <307322627@qq.com>
2020-06-12 10:40:59 +0800

    build: 构建工具更新

--- tests/unit/component/helloWorld.unit.js
+++ tests/unit/component/helloWorld.unit.js
@@ -1,12 +1,15 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
+    const localVue = createLocalVue()
     const wrapper = shallowMount(HelloWorld, {
+      localVue,
       propsData: { msg }
     })
-    expect(wrapper.text()).toMatch(msg)
+    const h1 = wrapper.find('h1')
+    expect(h1.text()).toMatch(msg)
   })
 })
